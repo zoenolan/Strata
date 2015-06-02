4a24bd7caacd0a9a6316030c7f090577367b573c
Chris Kent <chris@opengamma.com>
2015-06-02 13:01:45 +0100

    ParRates parallel shift

--- modules/function/src/test/java/com/opengamma/strata/function/marketdata/scenarios/curves/ParRatesParallelShiftTest.java
+++ modules/function/src/test/java/com/opengamma/strata/function/marketdata/scenarios/curves/ParRatesParallelShiftTest.java
@@ -12,8 +12,8 @@ import java.util.Map;
 import org.testng.annotations.Test;
 
 import com.google.common.collect.ImmutableMap;
-import com.opengamma.strata.basics.market.ObservableId;
 import com.opengamma.strata.collect.id.StandardId;
+import com.opengamma.strata.market.curve.CurveMetadata;
 import com.opengamma.strata.market.curve.ParRates;
 import com.opengamma.strata.market.id.QuoteId;
 
@@ -23,16 +23,16 @@ public class ParRatesParallelShiftTest {
   private static final String SCHEME = "test";
 
   public void absolute() {
-    Map<ObservableId, Double> rates = ImmutableMap.of(
+    Map<QuoteId, Double> rates = ImmutableMap.of(
         QuoteId.of(StandardId.of(SCHEME, "1")), 1d,
         QuoteId.of(StandardId.of(SCHEME, "2")), 2d,
         QuoteId.of(StandardId.of(SCHEME, "4")), 4d);
 
-    ParRates parRates = ParRates.of(rates);
+    ParRates parRates = ParRates.of(rates, CurveMetadata.of("curve"));
     ParRatesParallelShift shift = ParRatesParallelShift.absolute(0.1);
     ParRates shiftedParRates = shift.apply(parRates);
 
-    Map<ObservableId, Double> expectedRates = ImmutableMap.of(
+    Map<QuoteId, Double> expectedRates = ImmutableMap.of(
         QuoteId.of(StandardId.of(SCHEME, "1")), 1.1,
         QuoteId.of(StandardId.of(SCHEME, "2")), 2.1,
         QuoteId.of(StandardId.of(SCHEME, "4")), 4.1);
@@ -41,16 +41,16 @@ public class ParRatesParallelShiftTest {
   }
 
   public void relative() {
-    Map<ObservableId, Double> rates = ImmutableMap.of(
+    Map<QuoteId, Double> rates = ImmutableMap.of(
         QuoteId.of(StandardId.of(SCHEME, "1")), 1d,
         QuoteId.of(StandardId.of(SCHEME, "2")), 2d,
         QuoteId.of(StandardId.of(SCHEME, "4")), 4d);
 
-    ParRates parRates = ParRates.of(rates);
+    ParRates parRates = ParRates.of(rates, CurveMetadata.of("curve"));
     ParRatesParallelShift shift = ParRatesParallelShift.relative(0.1);
     ParRates shiftedParRates = shift.apply(parRates);
 
-    Map<ObservableId, Double> expectedRates = ImmutableMap.of(
+    Map<QuoteId, Double> expectedRates = ImmutableMap.of(
         QuoteId.of(StandardId.of(SCHEME, "1")), 1.1,
         QuoteId.of(StandardId.of(SCHEME, "2")), 2.2,
         QuoteId.of(StandardId.of(SCHEME, "4")), 4.4);
