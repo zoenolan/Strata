e0f12d42acdca83d2bba52b568e7b9a6d0cb51fc
Yuki Iwashita <yuki@opengamma.com>
2015-08-10 11:53:34 +0100

    Inflation end to end test, monthly

--- modules/pricer/src/test/java/com/opengamma/strata/pricer/rate/e2e/SwapInflationEnd2EndTest.java
+++ modules/pricer/src/test/java/com/opengamma/strata/pricer/rate/e2e/SwapInflationEnd2EndTest.java
@@ -7,9 +7,11 @@ package com.opengamma.strata.pricer.rate.e2e;
 
 import static com.opengamma.strata.basics.PayReceive.PAY;
 import static com.opengamma.strata.basics.PayReceive.RECEIVE;
+import static com.opengamma.strata.basics.currency.Currency.GBP;
 import static com.opengamma.strata.basics.currency.Currency.USD;
 import static com.opengamma.strata.basics.date.BusinessDayConventions.MODIFIED_FOLLOWING;
 import static com.opengamma.strata.basics.date.HolidayCalendars.SAT_SUN;
+import static com.opengamma.strata.basics.index.OvernightIndices.GBP_SONIA;
 import static com.opengamma.strata.basics.index.OvernightIndices.USD_FED_FUND;
 import static org.testng.Assert.assertEquals;
 
@@ -24,6 +26,7 @@ import org.testng.annotations.Test;
 import com.google.common.collect.ImmutableMap;
 import com.opengamma.analytics.financial.instrument.index.IndexPrice;
 import com.opengamma.analytics.financial.instrument.index.IndexPriceMaster;
+import com.opengamma.analytics.financial.interestrate.datasets.StandardDataSetsInflationGBP;
 import com.opengamma.analytics.financial.interestrate.datasets.StandardDataSetsInflationUSD;
 import com.opengamma.analytics.financial.interestrate.datasets.StandardTimeSeriesInflationDataSets;
 import com.opengamma.analytics.financial.model.interestrate.curve.PriceIndexCurveSimple;
@@ -62,6 +65,7 @@ import com.opengamma.strata.market.value.ValueType;
 import com.opengamma.strata.pricer.impl.Legacy;
 import com.opengamma.strata.pricer.rate.ImmutableRatesProvider;
 import com.opengamma.strata.pricer.rate.RatesProvider;
+import com.opengamma.strata.pricer.rate.swap.DiscountingSwapLegPricer;
 import com.opengamma.strata.pricer.rate.swap.DiscountingSwapProductPricer;
 
 /**
@@ -69,12 +73,12 @@ import com.opengamma.strata.pricer.rate.swap.DiscountingSwapProductPricer;
  */
 @Test
 public class SwapInflationEnd2EndTest {
-  private static final LocalDate VALUATION_DATE = LocalDate.of(2014, 10, 9);
   private static final ZonedDateTime CALIBRATION_DATE = DateUtils.getUTCDate(2014, 10, 9);
+  private static final LocalDate VALUATION_DATE = CALIBRATION_DATE.toLocalDate();
   private static final Pair<InflationProviderDiscount, CurveBuildingBlockBundle> MULTICURVE_INFL_1_PAIR = 
       StandardDataSetsInflationUSD.getCurvesUsdOisUsCpi(CALIBRATION_DATE);
   private static final InflationProviderDiscount MULTICURVE_INFL_1 = MULTICURVE_INFL_1_PAIR.getFirst();
-  static final PriceIndex US_CPI = PriceIndices.US_CPI_U;
+  private static final PriceIndex US_CPI = PriceIndices.US_CPI_U;
   private static final ZonedDateTimeDoubleTimeSeries HTS_CPI_OLD =
       StandardTimeSeriesInflationDataSets.timeSeriesUsCpi(CALIBRATION_DATE);
   // historical time series of price index
@@ -231,5 +235,118 @@ public class SwapInflationEnd2EndTest {
   public void test_agedSwap5Y() {
     MultiCurrencyAmount pv = PRICER.presentValue(SWAP_INFLATION_2, RATES_PROVIDER);
     assertEquals(pv.getAmount(USD).getAmount(), 557423.3816632524, NOTIONAL * TOL);
+  }
+
+
+  private static final PriceIndex GBP_RPI = PriceIndices.GB_RPI;
+
+  private static final ZonedDateTime STD_REFERENCE_DATE = DateUtils.getUTCDate(2014, 4, 11);
+  private static final Pair<InflationProviderDiscount, CurveBuildingBlockBundle> MULTICURVE_PAIR_STD = StandardDataSetsInflationGBP
+      .getCurvesGBPRpiAndSonia();
+  private static final InflationProviderDiscount INFLATION_MULTICURVE_STD = MULTICURVE_PAIR_STD.getFirst();
+  
+  private static final LocalDateDoubleTimeSeries TS_PRICE_INDEX_USD_WITH_TODAY = LocalDateDoubleTimeSeries.builder()
+      .put(LocalDate.of(2013, 12, 31), 253.4)
+      .put(LocalDate.of(2013, 12, 31), 253.4)
+      .put(LocalDate.of(2013, 12, 31), 253.4)
+      .build();
+
+  // price index curve
+  private static final InterpolatedNodalCurve CURVE_GB;
+  static {
+    IndexPrice[] indexList = StandardDataSetsInflationGBP.indexONArray();
+    IndexPrice gbpRpi = indexList[0];
+    PriceIndexCurveSimple indexCurve = (PriceIndexCurveSimple) INFLATION_MULTICURVE_STD
+        .getPriceIndexCurves().get(gbpRpi);
+    InterpolatedDoublesCurve curve = (InterpolatedDoublesCurve) indexCurve.getCurve();
+    DefaultCurveMetadata metaData = DefaultCurveMetadata.builder()
+        .curveName(CurveName.of(curve.getName()))
+        .xValueType(ValueType.MONTHS)
+        .yValueType(ValueType.PRICE_INDEX)
+        .build();
+    int nData = curve.getXDataAsPrimitive().length;
+    double[] xValueInt = new double[nData];
+    for (int i = 0; i < nData; ++i) {
+      //      System.out.println(curve.getXDataAsPrimitive()[i] + "\t" + (curve.getXDataAsPrimitive()[i] * 12d));
+      xValueInt[i] = (int) (curve.getXDataAsPrimitive()[i] * 12d);
+    }
+    CURVE_GB = InterpolatedNodalCurve.builder()
+        .extrapolatorLeft(new FlatExtrapolator1D())
+        .extrapolatorRight(new FlatExtrapolator1D())
+        .interpolator(new LinearInterpolator1D())
+        .metadata(metaData)
+        .xValues(xValueInt)
+        .yValues(curve.getYDataAsPrimitive())
+        .build();
+  }
+  // create rates provider
+  private static final RatesProvider RATES_PROVIDER_GB = ImmutableRatesProvider.builder()
+      .valuationDate(STD_REFERENCE_DATE.toLocalDate())
+      .fxMatrix(INFLATION_MULTICURVE_STD.getFxRates())
+      .discountCurves(Legacy.discountCurves(INFLATION_MULTICURVE_STD.getMulticurveProvider()))
+      .indexCurves(Legacy.indexCurves(INFLATION_MULTICURVE_STD.getMulticurveProvider()))
+      .priceIndexValues(
+          ImmutableMap.of(
+              GBP_RPI,
+              ForwardPriceIndexValues.of(GBP_RPI, YearMonth.from(STD_REFERENCE_DATE), TS_PRICE_INDEX_USD_WITH_TODAY,
+                  CURVE_GB)))
+      .timeSeries(ImmutableMap.of(GBP_SONIA, LocalDateDoubleTimeSeries.empty()))
+      .build();
+
+  // GBP inflation swap, monthly
+  private static final LocalDate START_DATE_3 = LocalDate.of(2014, 4, 4);
+  private static final LocalDate END_DATE_3 = LocalDate.of(2019, 4, 4);
+  private static final Frequency FREQUENCY_3 = Frequency.ofYears(5);
+  private static final RateCalculationSwapLeg INFLATION_MONTHLY_SWAP_LEG_REC_3 = RateCalculationSwapLeg.builder()
+      .payReceive(RECEIVE)
+      .accrualSchedule(PeriodicSchedule.builder()
+          .startDate(START_DATE_3)
+          .endDate(END_DATE_3)
+          .frequency(FREQUENCY_3)
+          .businessDayAdjustment(BusinessDayAdjustment.of(MODIFIED_FOLLOWING, SAT_SUN))
+          .build())
+      .paymentSchedule(PaymentSchedule.builder()
+          .paymentFrequency(FREQUENCY_3)
+          .paymentDateOffset(DaysAdjustment.NONE)
+          .build())
+      .calculation(InflationRateCalculation.builder()
+          .index(GBP_RPI)
+          .interpolated(false)
+          .lag(Period.ofMonths(3))
+          .build())
+      .notionalSchedule(NotionalSchedule.of(GBP, NOTIONAL))
+      .build();
+  private static final RateCalculationSwapLeg INFLATION_FIXED_SWAP_LEG_PAY_3 = RateCalculationSwapLeg.builder()
+      .payReceive(PAY)
+      .accrualSchedule(PeriodicSchedule.builder()
+          .startDate(START_DATE_3)
+          .endDate(END_DATE_3)
+          .frequency(Frequency.ofYears(1))
+          .businessDayAdjustment(BusinessDayAdjustment.of(MODIFIED_FOLLOWING, SAT_SUN))
+          .build())
+      .paymentSchedule(PaymentSchedule.builder()
+          .paymentFrequency(FREQUENCY_3)
+          .paymentDateOffset(DaysAdjustment.NONE)
+          .compoundingMethod(CompoundingMethod.STRAIGHT)
+          .build())
+      .notionalSchedule(NotionalSchedule.of(GBP, NOTIONAL))
+      .calculation(FixedRateCalculation.builder()
+          .rate(ValueSchedule.of(0.02506))
+          .dayCount(DayCounts.ONE_ONE)
+          .build())
+      .build();
+  private static final Swap SWAP_INFLATION_3 = Swap.builder()
+      .legs(INFLATION_MONTHLY_SWAP_LEG_REC_3, INFLATION_FIXED_SWAP_LEG_PAY_3)
+      .build();
+
+  DiscountingSwapLegPricer pricer = DiscountingSwapLegPricer.DEFAULT;
+
+  @Test(enabled = false)
+  // TODO investigate this
+  public void test() {
+    System.out.println(pricer.presentValue(INFLATION_FIXED_SWAP_LEG_PAY_3, RATES_PROVIDER_GB));
+    System.out.println(pricer.presentValue(INFLATION_MONTHLY_SWAP_LEG_REC_3, RATES_PROVIDER_GB));
+    MultiCurrencyAmount pv = PRICER.presentValue(SWAP_INFLATION_3, RATES_PROVIDER_GB);
+    System.out.println(pv);
   }
 }
